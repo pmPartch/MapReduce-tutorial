e1679dc47650b0a1c061e46eefb69cd0068f9af0
pmPartch <pmp@pmconsult.com>
2015-09-26 18:58:12 -0700

    fix output path

--- WordCount.java
+++ WordCount.java
@@ -11,7 +11,7 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.mapreduce.lib.input.TextOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 public class WordCount {
     public static class WordCountMap extends Mapper<Object, Text, Text, IntWritable> {
